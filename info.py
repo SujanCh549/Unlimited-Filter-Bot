58eacb54128ca5bb044446c148fb7e7f12cf70f8
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-06-25 20:33:58 +0545

    Update info.py

--- info.py
+++ info.py
@@ -46,13 +46,13 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Channel Button Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Sujan_BotZ')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Sujan_BotZ')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', CyniteSupport')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Sujan_BotZ')
 MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 
 # Custom Chats
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', 0))
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', 0))
-FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 't.me/Restricttest')
+FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 't.me/Sujan_BotZ')
 
 # Log Channels
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
