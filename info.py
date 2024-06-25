76a46b1cc4d22b7212affd502707e92db2b58a47
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-06-25 20:45:00 +0545

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ('API_ID', '25502576'))
+API_ID = environ('API_ID', '25502576')
 API_HASH = environ('API_HASH', 'f0f35dbb5b0081cdc8d3c9d5383c4628')
 BOT_TOKEN = environ('BOT_TOKEN', '6002778950:AAFq3Vw4l2pGiWXVqb6YHQJR2cxzYLLNMtY')
 
