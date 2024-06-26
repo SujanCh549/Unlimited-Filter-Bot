435767ec5ae97a744d9675f88df848806210eff8
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-06-26 16:57:53 +0545

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ NEWGRP = environ.get("NEWGRP", "https://graph.org/file/6d2e12a6d0dcd9261d909.jpg
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5123039648').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5123039648').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '6474527080').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
